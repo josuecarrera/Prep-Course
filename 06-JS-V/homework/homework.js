9d8d8c1fdde1c6c2aca6648fd8033506a2003c96
Josue <josuecarrera13@hotmail.com>
2022-07-21 16:12:52 -0500

    Six commint

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -83,11 +83,18 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+  //Tu código:
+  let persona = new Persona("Juan", "Perez", 22, "Saavedra 123");
+  return persona;
 }
   
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
+  //Tu código:
+  Persona.prototype.datos = function () {
+    return `${this.nombre}, ${this.edad} años`;
+  }
 }
   
 
