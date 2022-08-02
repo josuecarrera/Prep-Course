627d53b75578af0e2de0c2c3c8bcc435bff23933
Josue <josuecarrera13@hotmail.com>
2022-08-02 07:48:02 -0500

    JSextra commit

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  let matriz = [];
+  for (let key in objeto) {
+    matriz.push([key, objeto[key]]);
+  }
+  return matriz;
 }
 
 
